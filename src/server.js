93f050607fb796c89d93f82c9a0861ad72d13025
kieuishere <dvkieu165@gmail.com>
2024-04-01 17:27:41 +0700

    #31 Connect to MySQL

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -8,11 +9,19 @@ const hostname = process.env.HOST_NAME;
 
 const configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/webRoute');
+const connection = require('./config/database');
 
+//Config
 configViewEngine(app);
+
 // ROUTE
 app.use('/', webRouter);
 app.use('/home', webRouter);
+
+//MySQL
+connection.query('select * from users u', function (err, results, fields) {
+    console.log('>>>results = ', results);
+});
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}, host ${hostname}`);
